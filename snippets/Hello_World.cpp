3f755ac26d3d9eba1c15cfa153225459ac6044a6
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-10-01 20:10:53 +0530

    Update and rename Chocolate Game.cpp to Hello_World.cpp

---
+++ snippets/Hello_World.cpp
@@ -0,0 +1,6 @@
+#include <iostream>                 // we include the iostream library, which contains the input and output functions.
+using namespace std;                // we use the standard namespace, which contains the cout function.
+int main() {                        // the main function is the entry point of the program.
+   cout << "Hello, World!" << endl; // This prints Hello, World!
+   return 0;                        // This returns 0 to the operating system.
+}                                   // This ends the main function.
